1c1ee2c87737bc458470c57a61a5ff2035d18688
Jose Gabriel Tamura L <jg.tamura10@uniandes.edu.co>
2017-05-16 19:30:30 -0500

    avances

--- imports/ui/canvasSuperiorAMapa.jsx
+++ imports/ui/canvasSuperiorAMapa.jsx
@@ -28,15 +28,20 @@ export default class CanvasSuperiorAMapa extends Component {
 
           var listaDeHashtags = coordenadasActuales[3];
           for(var j=0;j< listaDeHashtags.length;j++){
-            var hashtagActual = listaDeHashtags[j];
-            this.hashDeHashtags.hashtagActual = [{'x':coordenadasActuales[0], 'y':coordenadasActuales[1]}].push(this.hashDeHashtags.hashtagActual);
+            var hashtagActual = listaDeHashtags[j].text;
+            this.hashDeHashtags.hashtagActual = [{'x':coordenadasActuales[0], 'y':coordenadasActuales[1]}].concat(this.hashDeHashtags.hashtagActual);
+            console.log("el hashtag: ");
+            console.log(hashtagActual);
+            console.log("Tiene la siguiente lista de coordenadas: ");
+            console.log(this.hashDeHashtags.hashtagActual);
           }
-
-          ctx.beginPath();
-          ctx.arc(coordenadasActuales[0],coordenadasActuales[1],2,0,2*Math.PI,true);
-          ctx.stroke();
-          ctx.fillStyle = 'black';
-          ctx.fill();
+          if(listaDeHashtags.length === 0){
+            ctx.beginPath();
+            ctx.arc(coordenadasActuales[0],coordenadasActuales[1],2,0,2*Math.PI,true);
+            ctx.stroke();
+            ctx.fillStyle = 'black';
+            ctx.fill();
+          }
 
           if(coordenadasActuales[2] > mayorLocal){
             mayorLocal = coordenadasActuales[2];
@@ -46,6 +51,26 @@ export default class CanvasSuperiorAMapa extends Component {
       if(mayorLocal > this.maxId){
         this.maxId = mayorLocal;
       }
+    }
+    this.dibujarClusterHash();
+  }
+  dibujarClusterHash(){
+    var c=document.getElementById("myCanvas");
+    var ctx=c.getContext("2d");
+    //por cada atributo en hashDeHashtags (o sea cada hashtag que han creado)
+    //va a crear un cluster con todos los tweets que lo mencionaron
+    for (var property in this.hashDeHashtags) {
+        if (this.hashDeHashtags.hasOwnProperty(property)) {
+          ctx.beginPath();
+          //property es una lista con los x y
+          for(var i=0; i< property.length;i++){
+            var coordenadasActuales = property[i];
+            ctx.arc(coordenadasActuales.x,coordenadasActuales.y,2,0,2*Math.PI,true);
+            ctx.stroke();
+            ctx.fillStyle = 'black';
+            ctx.fill();
+          }
+        }
     }
 
   }
